4ee07b838f9a289438f6f3b5fdba293109e0af00
N9 <nguyengiapham1@gmail.com>
2025-06-20 08:50:04 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,21 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'N9 Development'
+author 'N9 Anti-DDoS System by ChatGPT'
 description 'Full-featured N9 Anti-DDoS with bandwidth enforcement, SQLite logging, geoIP, webhook, fail2ban'
 version '5.0.0'
 
 server_scripts {
-    '@oxmysql/lib/MySQL.lua', -- Optional if you use MySQL
+    '@oxmysql/lib/MySQL.lua', -- Optional if you use MySQL instead of SQLite
     'config.lua',
-    'server/banlist.lua',
     'server/db.lua',
+    'server/banlist.lua',
     'server/geoip.lua',
     'server/discord.lua',
     'server/fingerprint.lua',
     'server/main.lua'
+}
+
+client_scripts {
+    'client/main.lua'
 }
